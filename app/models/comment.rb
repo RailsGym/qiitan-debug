efb81f13c17e3f471fafabca84a4e5cf0ee161a9
shun <3939s.w@gmail.com>
2020-02-14 21:08:32 +0900

    comment notifable アソシエーションバグ修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article
+  
 
+  has_many :notifications, dependent: :destroy, as: :notifiable, inverse_of: :notifiable
+  
   validates :body, presence: true
 end
